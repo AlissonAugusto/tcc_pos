59ddec278c453ec84fb166ffe66b00f499d8f967
alisson augusto silva <email.alisson@hotmail.com>
2019-02-16 00:06:02 -0200

    First commit

---
+++ Assets/Scripts/answerMovimentController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class answerMovimentController : MonoBehaviour
+{
+    public TextMeshPro value;
+
+    public Sprite blueColor;
+    public Sprite redColor;
+    public Sprite greenColor;
+    public Sprite yellowColor;
+
+    float velocity;
+    int answerValue;
+    int score;
+    int life;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        answerValue = 0;
+        velocity = ((float)(Mathf.Log(10) * 17.942) + UnityEngine.Random.Range(8, 13)) / 50;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        life = Int32.Parse(value.text);
+        score = gameController.score;
+        if (score > 0)
+        {
+            velocity = ((float)(Mathf.Log(score + 10) * 17.942) + UnityEngine.Random.Range(8, 13)) / 50;
+        }
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, Screen.height), velocity * Time.deltaTime);
+    }
+
+    public void OnMouseDown()
+    {
+        if (answerValue == 0)
+        {
+            answerValue = life;
+        }
+        life--;
+        value.text = life.ToString();
+        if (life <= 0)
+        {
+            gameObject.SetActive(false);
+            Handheld.Vibrate();
+            gameController.lastAnswerDestroyed = answerValue;
+            answerValue = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/gameController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameController : MonoBehaviour
+{
+    public static int lastAnswerDestroyed = 0;
+    public static int score = 0;
+
+    public GameObject firstAnswer;
+    public GameObject secondAnswer;
+    public GameObject thirdAnswer;
+    public TextMeshPro firstAnswerText;
+    public TextMeshPro secondAnswerText;
+    public TextMeshPro thirdAnswerText;
+
+    public Text firstNumberText;
+    public Text resultText;
+    public Text scoreText;
+
+    public int firstNumber;
+    public int secondNumber = 0;
+    public int result;
+
+    private int rightAnswer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        setAnswers();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = score.ToString();
+        if (lastAnswerDestroyed != 0 && lastAnswerDestroyed == secondNumber)
+        {
+            lastAnswerDestroyed = 0;
+            score += 1;
+            setAnswers();
+        }
+    }
+
+    public void setAnswers()
+    {
+        firstAnswer.transform.position = new Vector2(-2, Random.Range(-10.0F, -7.0F));
+        secondAnswer.transform.position = new Vector2(0, Random.Range(-10.0F, -7.0F));
+        thirdAnswer.transform.position = new Vector2(2, Random.Range(-10.0F, -7.0F));
+
+        firstAnswer.SetActive(true);
+        secondAnswer.SetActive(true);
+        thirdAnswer.SetActive(true);
+
+        firstNumber = Random.Range(1, 10);
+        secondNumber = Random.Range(1, 10);
+
+        result = firstNumber * secondNumber;
+
+        firstNumberText.text = firstNumber.ToString();
+        resultText.text = result.ToString();
+
+        rightAnswer = Random.Range(1, 4);
+        switch (rightAnswer)
+        {
+            case 1:
+                firstAnswerText.text = secondNumber.ToString();
+                secondAnswerText.text = Random.Range((Random.Range(1, rightAnswer)), (Random.Range(rightAnswer, 10))).ToString();
+                thirdAnswerText.text = Random.Range((Random.Range(1, rightAnswer)), (Random.Range(rightAnswer, 10))).ToString();
+                break;
+            case 2:
+                firstAnswerText.text = Random.Range((Random.Range(1, rightAnswer)), (Random.Range(rightAnswer, 10))).ToString();
+                secondAnswerText.text = secondNumber.ToString();
+                thirdAnswerText.text = Random.Range((Random.Range(1, rightAnswer)), (Random.Range(rightAnswer, 10))).ToString();
+                break;
+            case 3:
+                firstAnswerText.text = Random.Range((Random.Range(1, rightAnswer)), (Random.Range(rightAnswer, 10))).ToString();
+                secondAnswerText.text = Random.Range((Random.Range(1, rightAnswer)), (Random.Range(rightAnswer, 10))).ToString();
+                thirdAnswerText.text = secondNumber.ToString();
+                break;
+        }
+    }
+}
